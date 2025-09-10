25e2c592a4276b6ceccd806d3d727c222105a76a
Brittani Lovelace <blovelace84@gmail.com>
2025-09-10 19:45:39 -0400

    delete

--- massage-booking-app/src/services/appwrite.js
+++
@@ -1,8 +0,0 @@
-import { Client, Account, ID } from "react-native-appwrite";
-
-const client = new Client()
-  .setEndpoint("https://nyc.cloud.appwrite.io/v1") // Your Appwrite endpoint
-  .setProject("68bac70b0030dad2ef3f"); // Your project ID
-
-export const account = new Account(client);
-export { ID };
