529728fc224218e29953ed94d48a89c5cb019fb5
Brittani Lovelace <blovelace84@gmail.com>
2025-09-13 13:48:03 -0400

    Trying to link my signup and login components to navigate

--- massage-booking-app/src/App.jsx
+++ massage-booking-app/src/App.jsx
@@ -1,24 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import HomeClient from "./pages/HomeClient";
-import HomeTherapist from "./pages/HomeTherapist";
+// import HomeClient from "./pages/HomeClient";
+// import HomeTherapist from "./pages/HomeTherapist";
 import { auth } from "./services/firebase.config";
 const App = () => {
+
   useEffect(() => {
     console.log("Firebase Auth Ready!", auth);
   }, []);
 
   return (
-    <Router>
+    <div>
+      <nav>
+        <Link to="/signup">Signup</Link> | <Link to ="/login">Login</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home-client" element={<HomeClient />} />
-        <Route path="/home-therapist" element={<HomeTherapist />} />
+
+        {/* fallback route */}
+        <Route path="*" element={<h1>Page not found</h1>} />
+        {/* <Route path="/home-client" element={<HomeClient />} />
+        <Route path="/home-therapist" element={<HomeTherapist />} /> */}
       </Routes>
-    </Router>
+    </div>
   );
 }
 
--- massage-booking-app/src/services/firebase.config.js
+++ massage-booking-app/src/services/firebase.config.js
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyAdOWL5UL1NSshnyfx4adnS0wVxk-6x-yY",
   authDomain: "massage-booking-app-6e92e.firebaseapp.com",
-  projectId: "massage-booking-app-6e92e.firebaseapp.com",
+  projectId: "massage-booking-app-6e92e",
   storageBucket: "massage-booking-app-6e92e.firebasestorage.app",
   messagingSenderId: "690396229197",
   appId: "1:690396229197:web:0c645ba8fb6e2726f53604"
